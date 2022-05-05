bbd6c804b4660b3b4aadd4c329e25b47b3dc891b
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-05-05 11:34:33 +0530

    [VINAY] Add: displaying number of Duplicate Word and Number Of unique words in the given Sentence

---
+++ Non_LinearDataStructure/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Non_LinearDataStructure;
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        MyMapNode<string, int> hash = new MyMapNode<string, int>(5);
+        string words = "to be or not to be";
+        string[] arr = words.Split(' ');
+        LinkedList<string> checkForDuplication = new LinkedList<string>();
+        foreach (string element in arr) // to -> be
+        {
+            int count = 0;
+            foreach (string match in arr) //to->be->or->not->to-> be-> 
+            {
+                if (element == match)
+                {
+                    count++;//1->2
+                    if (checkForDuplication.Contains(element))
+                    {
+                        break;
+                    }
+                }
+
+            }
+
+            if (!checkForDuplication.Contains(element))
+            {
+                checkForDuplication.AddLast(element);
+                hash.Add(element, count);//(to,2) 
+            }
+
+        }
+        hash.Display();
+    }
+}
